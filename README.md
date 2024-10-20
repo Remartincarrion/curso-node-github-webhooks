89321340a406b5306750febcae7a153e0508119a
Rodolfo <remartincarrion@gmail.com>
2024-10-20 11:45:08 -0300

    Fin seccion 24

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # curso-node-github-webhooks
 Un proyecto para probar webhooks de github + discord
+
+
+
+
+1. Clonar el repositorio
+2. Clonar .env.template a .env
+3. Ejecutar `npm install`
+4. Ejecutar `npm run dev`
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
-
+import { GithubSha256Middleware } from './presentation/middlewares/github-sha256.middleware';
 
 
 
@@ -20,8 +20,12 @@ function main() {
     // middleware
     app.use( express.json() );
 
+    app.use( GithubSha256Middleware.verifySignature );
+
+
     app.post('/api/github', controller.webhookHandler );
 
+    
     app.listen(envs.PORT, () => {
         console.log(`App running on port ${ envs.PORT }`)
     });
